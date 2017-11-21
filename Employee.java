8c3676f0bb682092f7e3885ba59120073fbc68d9
Shiela Intertas <intertasshiela@gmail.com>
2017-11-21 20:47:07 +0800

    Added name field

--- Employee.java
+++ Employee.java
@@ -1,17 +1,27 @@
 public class Employee {
 
 	private int employeeId;
+	private String name;
 	
 	public Employee(){
 		this.employeeId = 1;
+		this.name ="Andres Bonifacio";
 	}
 	
 	public void setEmployeeId (int employeeId){
 		this.employeeId = employeeId;
 	}
 	
-	public int getemployeeId(){
+	public int getEmployeeId(){
 		return employeeId;
+	}
+	
+	public void setName (String name){
+		this.name = name;
+	}
+	
+	public int getName(){
+		return name;
 	}
 	
 }
