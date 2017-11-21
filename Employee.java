c0429f06518a2e1a8b65a12eab6036a874274492
Shiela Intertas <intertasshiela@gmail.com>
2017-11-21 19:41:26 +0800

    Added Employee bean

---
+++ Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+
+	private int employeeId;
+	
+	public Employee(){
+		this.employeeId = 1;
+	}
+	
+	public void setEmployeeId (int employeeId){
+		this.employeeId = employeeId;
+	}
+	
+	public int getemployeeId(){
+		return employeeId;
+	}
+	
+}
